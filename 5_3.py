097a72bf0d9c87276606684bcec508745c3bfdfe
Fedir Yarovyi <jarfed2@gmail.com>
2019-05-13 16:55:10 +0300

    Update 5_3.py

--- 5_3.py
+++ 5_3.py
@@ -32,3 +32,20 @@ def super_fibonacci(n,m):
         First_list.append(sum(First_list[len(First_list)-m:]))
     return First_list[n-1]
 print(super_fibonacci(n,m))
+
+
+########################################## 2 Варіант ##########################################
+def super_fibonacci(n, m):
+    # за умовою перші m елементів - одиниці
+    if n<=m:
+        return 1
+    # інакше доведеться рахувати
+    else:
+        # ініціалізуємо змінну для суми
+        sum = 0
+        # і додаємо m попередніх членів послідовності, для розрахунку кожного з них рекурсивно викликаємо нашу функцію
+        for i in range(1, m+1):
+            previous = super_fibonacci(n-i, m)
+            sum = sum + previous
+        # повертаємо суму, яка є значенням n-го елементу
+        return sum
